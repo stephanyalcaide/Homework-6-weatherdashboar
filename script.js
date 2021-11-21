4b54b2a737610b8b1ba4789b7a276b02b6872f84
Stephany Alcaide <81532183+stephanyalcaide@users.noreply.github.com>
2021-11-20 19:11:21 -0600

    Update script.js

--- script.js
+++ script.js
@@ -1,57 +1,68 @@
 
 // dia spanish for day
-function Formatdia(date){
+function Formatdia (date){
     var date = new Date();
     console.log(date);
     var month = date.getMonth()+1;
-    var dia = date.getDate();
+    var dia  = date.getDate();
     
     var diaOutput = date.getFullYear() + '/' +
         (month<10 ? '0' : '') + month + '/' +
-        (dia<10 ? '0' : '') + dia;
+        (dia <10 ? '0' : '') + dia ;
     return diaOutput;
 }
-//variables
-var townLog =$("#town-log");
-var town = [];
+
+var townList =$("#town-list");
+var cities = [];
 var key = "fc8bffadcdca6a94d021c093eac22797";
 
 
 
+
+//Calling function init();
 init();
 
+//Function init();
 function init(){
-    
-    var storedtown = JSON.parse(localStorage.getItem("town"));
+    //Get stored cities from localStorage
+    //Parsing the JSON string to an object
+    var storedCities = JSON.parse(localStorage.getItem("cities"));
 
-    
-    if (storedtown !== null) {
-        town = storedtown;
+    // If cities were retrieved from localStorage, update the cities array to it
+    if (storedCities !== null) {
+        cities = storedCities;
       }
-    
-    rendertown();
+    // Render cities to the DOM
+    renderCities();
+    // console.log(cities);
 }
 
-//Function Storetown()
-function storetown(){
-  localStorage.setItem("town", JSON.stringify(town));
+//Function StoreCities()
+function storeCities(){
+   // Stringify and set "cities" key in localStorage to cities array
+  localStorage.setItem("cities", JSON.stringify(cities));
   console.log(localStorage);
 }
 
-function rendertown() {
-    townLog.empty();
+//Function renderCities()
+function renderCities() {
+    // Clear townList element
+    // townList.text = "";
+    // townList.HTML = "";
+    townList.empty();
     
-    
-    for (var i = 0; i < town.length; i++) {
-      var town = town[i];
+    // Render a new li for each town
+    for (var i = 0; i < cities.length; i++) {
+      var town = cities[i];
       
       var li = $("<li>").text(town);
       li.attr("id","listC");
       li.attr("data-town", town);
       li.attr("class", "list-group-item");
       console.log(li);
-      townLog.prepend(li);
+      townList.prepend(li);
     }
+    //Get Response weather for the first town only
     if (!town){
         return
     } 
@@ -60,40 +71,50 @@ function rendertown() {
     };
 }   
 
+  //When form is submitted...
   $("#add-town").on("click", function(event){
       event.preventDefault();
 
+    // This line will grab the town from the input box
     var town = $("#town-input").val().trim();
     
+    // Return from function early if submitted town is blank
     if (town === "") {
         return;
     }
-    town.push(town);
-  storetown();
-  rendertown();
+    //Adding town-input to the town array
+    cities.push(town);
+    // Store updated cities in localStorage, re-render the list
+  storeCities();
+  renderCities();
   });
+
+  //Function get Response Weather 
   
   function getResponseWeather(townName){
     var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=" +townName+ "&appid=" + key; 
 
-    //Clear content of todia-weather
-    $("#todia-weather").empty();
+    //Clear content of todia -weather
+    $("#todia -weather").empty();
     $.ajax({
       url: queryURL,
       method: "GET"
     }).then(function(response) {
         
-      townTitle = $("<h3>").text(response.name + " "+ Formatdia());
-      $("#todia-weather").append(townTitle);
+      // Create a new table row element
+      townTitle = $("<h3>").text(response.name + " "+ Formatdia ());
+      $("#todia -weather").append(townTitle);
       var TempetureToNum = parseInt((response.main.temp)* 9/5 - 459);
-      var townWeather = $("<p>").text("Tempeture: "+ TempetureToNum + " °F");
-      $("#todia-weather").append(townWeather);
+      var townTemperature = $("<p>").text("Tempeture: "+ TempetureToNum + " °F");
+      $("#todia -weather").append(townTemperature);
       var townHumidity = $("<p>").text("Humidity: "+ response.main.humidity + " %");
-      $("#todia-weather").append(townHumidity);
+      $("#todia -weather").append(townHumidity);
       var townWindSpeed = $("<p>").text("Wind Speed: "+ response.wind.speed + " MPH");
-      $("#todia-weather").append(townWindSpeed);
-      
+      $("#todia -weather").append(townWindSpeed);
+      var CoordLon = response.coord.lon;
       var CoordLat = response.coord.lat;
+    
+        //Api to get UV index
         var queryURL2 = "https://api.openweathermap.org/data/2.5/uvi?appid="+ key+ "&lat=" + CoordLat +"&lon=" + CoordLon;
         $.ajax({
             url: queryURL2,
@@ -102,7 +123,7 @@ function rendertown() {
             var townUV = $("<span>").text(responseuv.value);
             var townUVp = $("<p>").text("UV Index: ");
             townUVp.append(townUV);
-            $("#todia-weather").append(townUVp);
+            $("#todia -weather").append(townUVp);
             console.log(typeof responseuv.value);
             if(responseuv.value > 0 && responseuv.value <=2){
                 townUV.attr("class","green")
@@ -121,32 +142,32 @@ function rendertown() {
             }
         });
     
-        //Api to get 5-dia forecast  
+        //Api to get 5-dia  forecast  
         var queryURL3 = "https://api.openweathermap.org/data/2.5/forecast?q=" + townName + "&appid=" + key;
             $.ajax({
             url: queryURL3,
             method: "GET"
-        }).then(function(response5dia) { 
+        }).then(function(response5dia ) { 
             $("#boxes").empty();
-            console.log(response5dia);
+            console.log(response5dia );
             for(var i=0, j=0; j<=5; i=i+6){
-                var read_date = response5dia.list[i].dt;
-                if(response5dia.list[i].dt != response5dia.list[i+1].dt){
+                var read_date = response5dia .list[i].dt;
+                if(response5dia .list[i].dt != response5dia .list[i+1].dt){
                     var FivediaDiv = $("<div>");
                     FivediaDiv.attr("class","col-3 m-2 bg-primary")
-                    var d = new Date(0); 
+                    var d = new Date(0); // The 0 there is the key, which sets the date to the epoch
                     d.setUTCSeconds(read_date);
                     var date = d;
                     console.log(date);
                     var month = date.getMonth()+1;
-                    var dia = date.getDate();
+                    var dia  = date.getDate();
                     var diaOutput = date.getFullYear() + '/' +
                     (month<10 ? '0' : '') + month + '/' +
-                    (dia<10 ? '0' : '') + dia;
+                    (dia <10 ? '0' : '') + dia ;
                     var Fivediah4 = $("<h6>").text(diaOutput);
-                    
+                    //Set src to the imags
                     var imgtag = $("<img>");
-                    var skyconditions = response5dia.list[i].weather[0].main;
+                    var skyconditions = response5dia .list[i].weather[0].main;
                     if(skyconditions==="Clouds"){
                         imgtag.attr("src", "https://img.icons8.com/color/48/000000/cloud.png")
                     } else if(skyconditions==="Clear"){
@@ -155,17 +176,17 @@ function rendertown() {
                         imgtag.attr("src", "https://img.icons8.com/color/48/000000/rain.png")
                     }
 
-                    var pWeatherK = response5dia.list[i].main.temp;
+                    var pTemperatureK = response5dia .list[i].main.temp;
                     console.log(skyconditions);
-                    var TempetureToNum = parseInt((pWeatherK)* 9/5 - 459);
-                    var pWeather = $("<p>").text("Tempeture: "+ TempetureToNum + " °F");
-                    var pHumidity = $("<p>").text("Humidity: "+ response5dia.list[i].main.humidity + " %");
+                    var TempetureToNum = parseInt((pTemperatureK)* 9/5 - 459);
+                    var pTemperature = $("<p>").text("Tempeture: "+ TempetureToNum + " °F");
+                    var pHumidity = $("<p>").text("Humidity: "+ response5dia .list[i].main.humidity + " %");
                     FivediaDiv.append(Fivediah4);
                     FivediaDiv.append(imgtag);
-                    FivediaDiv.append(pWeather);
+                    FivediaDiv.append(pTemperature);
                     FivediaDiv.append(pHumidity);
                     $("#boxes").append(FivediaDiv);
-                    console.log(response5dia);
+                    console.log(response5dia );
                     j++;
                 }
             
@@ -178,6 +199,7 @@ function rendertown() {
     
   }
 
+  //Click function to each Li 
   $(document).on("click", "#listC", function() {
     var thistown = $(this).attr("data-town");
     getResponseWeather(thistown);
